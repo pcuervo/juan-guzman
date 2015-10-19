bd16c39f532ae1b6a0416a587c6ec72380933600
mago <mago_hdz_h@outlook.es>
2015-10-19 14:46:09 -0500

    avance del fin de semana

--- themes/juan-guzman/inc/pages.php
+++ themes/juan-guzman/inc/pages.php
@@ -8,16 +8,16 @@
 add_action('init', function(){
 
 	// HOME
-	// if( ! get_page_by_path('inicio') ){
-	// 	$page = array(
-	// 		'post_author' => 1,
-	// 		'post_status' => 'publish',
-	// 		'post_title'  => 'Inicio',
-	// 		'post_name'   => 'inicio',
-	// 		'post_type'   => 'page'
-	// 	);
-	// 	wp_insert_post( $page, true );
-	// }
+	if( ! get_page_by_path('mapa') ){
+		$page = array(
+			'post_author' => 1,
+			'post_status' => 'publish',
+			'post_title'  => 'mapa',
+			'post_name'   => 'mapa',
+			'post_type'   => 'page'
+		);
+		wp_insert_post( $page, true );
+	}
 
 	
 });
