4e2ed0ff2344b1ba075a11b53378aeb8fd08823a
mago <mago_hdz_h@outlook.es>
2015-10-20 16:00:40 -0500

    acomodo del home imagen-subtitulo-texto-flecha

--- themes/juan-guzman/index.php
+++ themes/juan-guzman/index.php
@@ -29,7 +29,7 @@
 
 	<section>
 		<div class="[ relative ]">
-			<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img3.jpg">
+			<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img5.jpg">
 			<a class="[ btn btn-primary btn-squared ][ absolute absolute-bottom--0 absolute-left--0 ]" href="#">
 				Mapa
 			</a>
@@ -39,34 +39,94 @@
 				<span class="[ border-bottom border-bottom--primary ]">HANS</span> 
 				<span class="[ border-bottom border-bottom--primary ]">GUTMANN</span>
 			</h2>
-			<p class="[ text-center padding--10 ] [ color-light ]">El primer nombre de Juan Guzmàn fue Hans Gutmann. Nacido en <u>Colonia, Alemania,</u> en 1911. Hizo estudios en ingenierìa metalùrgica.</p>
+			<p class="[ text-center padding--10 ] [ color-light ]">El primer nombre de Juan Guzman fue Hans Gutmann. Nacido en <u>Colonia, Alemania,</u>en 1911, hizo estudios en ingeniería metalúrgica. La situación económica de su país natal no fue propicia para su desenvolvimiento en esa disciplina. Las producciones de una pequeña compañía cinematográfica le dieron oportunidad de aprender otros oficios. Por esa vía inició su relación con la fotografía, que pocos años después habría de ejercer como principal profesión y como herramienta al servicio de las causas políticas de las que fue seguidor. </p>
 			<span class="[ color-brand-primary ]">&#8595;</span>
 		</div>
 	</section>
 
 	<section>
-		<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img2.jpg">
-		<div class="padding--10">
-			<div class="[ row ]">
-				<div class="[ col-xs-12 ]">
+		<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img7.jpg">
+		<div class="padding">
+		<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
 					<h2 class="[ color-gray ] [ text-center ]">
-						<span class="[ border-bottom border-bottom--primary ]">SEGUNDA </span>
-						<span class="[ border-bottom border-bottom--primary ]">GUERRA </span>
-						<span class="[ border-bottom border-bottom--primary ]">MUNDIAL</span>
-					</h2>
+						<span class="[ border-bottom border-bottom--primary ]">EUROPA</span>
+					</h2>		
 				</div>
 			</div>
 			<div class="[ row ]">
 				<div class="[ col-xs-12 text-center]">
 					<div class="[ text-center ]">
-						<p class="[ padding--10 ]">La situaciòn econòmica de su paìs natal no fue propicia para su desenvlvimiento en esa disciplina. Las producciones de una pequeña compañìa cinematogràfica le dieron oportunidad de aprender otros oficios.</p>
+						<p class="[ padding--10 ]">El ascenso al poder de Adolfo Hitler, en 1933, obligó a Gutmann a salir al exilio. Vivió y trabajó en Francia, recorrió Italia y Grecia, antes de viajar en 1936 a España, país en el que fue testigo y protagonista de la guerra civil que desató la rebelión militar comandada por Francisco Franco en contra de la II República.</p>
 						<span class="[ color-brand-primary text-center ]">&#8595;</span>
 					</div>		
 				</div>
 			</div>
 		</div>
 	</section>
-	
+
+	<section>
+		<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img6.jpg">
+		<div class="[ padding ]">
+			<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
+					<h2 class="[ color-gray ] [ text-center ]">
+						<span class="[ border-bottom border-bottom--primary ]">ESPAÑA</span>
+					</h2>		
+				</div>
+			</div>
+			<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
+					<div class="[ text-center ]">
+						<p class="[ padding--10 ]">Se sumó a las fuerzas republicanas, fotografió sus movilizaciones en varios frentes y obtuvo el grado de capitán de ingenieros. Ya rebautizado como Juan Guzmán, contrajo matrimonio con una ciudadana española, viviendo junto con ella las vicisitudes de la derrota republicana. </p>
+						<span class="[ color-brand-primary text-center ]">&#8595;</span>
+					</div>		
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+
+		<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img8.jpg">
+		<div class="padding">
+			<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
+					<h2 class="[ color-gray ] [ text-center ]">
+						<span class="[ border-bottom border-bottom--primary ]">MÈXICO</span>
+					</h2>		
+				</div>
+			</div>
+			<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
+					<div class="[ text-center ]">
+						<p class="[ padding--10 ]">Guzmán llegó a México en 1939, como parte de la comunidad de los refugiados españoles. A fines de ese año se integró al medio fotoperiodístico mexicano, del que fue parte activa en las siguientes tres décadas. Trabajó asimismo como corresponsal de la compañía estadounidense Time-Life. Novedades, Así, Hoy, Mañana, Sucesos para todos, Tiempo y Life en español, publicaron las imágenes y reportajes en que Guzmán hizo la crónica de las múltiples transformaciones de un país sometido a los impulsos, influjos y encandilamientos de la modernidad. </p>
+						<span class="[ color-brand-primary text-center ]">&#8595;</span>
+					</div>		
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<img class="[ img-responsive img-background-home ] " src="<?php echo THEMEPATH; ?>img/img9.jpg">
+		<div class="padding">
+			<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
+					<h2 class="[ color-gray ] [ text-center ]">
+						<span class="[ border-bottom border-bottom--primary ]">METROPOLI</span>
+					</h2>		
+				</div>
+			</div>
+			<div class="[ row ]">
+				<div class="[ col-xs-12 text-center]">
+					<div class="[ text-center ]">
+						<p class="[ padding--10 ]">La capital mexicana, magno escenario en que se confrontaban las tensiones entre lo tradicional y lo vanguardista, fue tema de muchas de las correrías fotográficas de Juan Guzmán, quien murió en esta urbe en 1982. El título de esta muestra procede de un reportaje publicado en la revista Mañana en 1952 y las imágenes que la conforman del archivo que por decisión de Teresita Miranda, última pareja de Guzmán, se integró a la colección fotográfica de Fundación Televisa.</p>
+					</div>		
+				</div>
+			</div>
+		</div>
+	</section>
 	
 
 <?php get_footer(); ?>
