2facf4a38dbf1c79fa72785e1d867a94d88243ae
Raúl De Zamacona <raul@zamacona.me>
2015-10-21 14:50:51 -0500

    if aerial/sabias_que

--- themes/juan-guzman/single.php
+++ themes/juan-guzman/single.php
@@ -6,6 +6,7 @@
 	$image 		= wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 	$fecha 		= get_fecha( $post->ID );
 	$lugar 		= get_lugar( $post->ID );
+	$is_aerial 	= get_vista_aerea( $post->ID );
 	$sabias_que = get_sabias_que( $post->ID );
 
 ?>
@@ -59,21 +60,29 @@
 
 		</div><!-- clearfix -->
 
-		<div class="[ clearfix ][ margin-bottom--large ]">
-			<section class="[ street-view ][ relative ][ no-padding ][ col-xs-10 col-sm-8 col-md-6 ][ col-xs-offset-1 col-sm-offset-2 col-md-offset-3 ]">
-				<div class="[ street-view-img ]"></div>
-				<a class="[ btn btn-primary btn-sm ][ absolute absolute-top--0 absolute-right--0 ]" href="#">
-					<i class="[ fa fa-street-view fa-2x ]"></i>
-				</a>
-			</section>
-		</div><!-- clearfix -->
+		<?php if ( $is_aerial == false ){ ?>
 
-		<div class="[ clearfix ][ margin-bottom--large ]">
-			<section class="[ text-center ][ col-xs-10 col-sm-8 col-md-6 ][ col-xs-offset-1 col-sm-offset-2 col-md-offset-3 ]">
-				<p><em>#SabíasQue</em>&nbsp;&nbsp;&nbsp;&nbsp; <a href="#"><i class="[ fa fa-twitter ]"></i></a> </p>
-				<p><?php echo $sabias_que; ?></p>
-			</section>
-		</div><!-- clearfix -->
+			<div class="[ clearfix ][ margin-bottom--large ]">
+				<section class="[ street-view ][ relative ][ no-padding ][ col-xs-10 col-sm-8 col-md-6 ][ col-xs-offset-1 col-sm-offset-2 col-md-offset-3 ]">
+					<div class="[ street-view-img ]"></div>
+					<a class="[ btn btn-primary btn-sm ][ absolute absolute-top--0 absolute-right--0 ]">
+						<i class="[ fa fa-street-view fa-2x ]"></i>
+					</a>
+				</section>
+			</div><!-- clearfix -->
+
+		<?php } ?>
+
+		<?php if ( '' !== $sabias_que ){ ?>
+
+			<div class="[ clearfix ][ margin-bottom--large ]">
+				<section class="[ text-center ][ col-xs-10 col-sm-8 col-md-6 ][ col-xs-offset-1 col-sm-offset-2 col-md-offset-3 ]">
+					<p><em>#SabíasQue</em>&nbsp;&nbsp;&nbsp;&nbsp; <a href="#"><i class="[ fa fa-twitter ]"></i></a> </p>
+					<p><?php echo $sabias_que; ?></p>
+				</section>
+			</div><!-- clearfix -->
+
+		<?php } ?>
 
 	</div>
 
